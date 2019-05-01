6c87a43901cf9d0a7cb2052898918441808c06b2
rudi hinds <rudihinds@gmail.com>
2019-05-01 10:30:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,5 +39,9 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  return array.delete()
+  return array.delete(string)
+end
+
+def using_delete_at(array, number)
+  return array.delete[number]
 end
