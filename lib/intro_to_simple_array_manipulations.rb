1289cae62922abcdf797f62a4f98ed1b3a44e258
rudi hinds <rudihinds@gmail.com>
2019-05-01 10:11:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,15 @@
 def using_push(array, string)
   return array.push(string)
 end
+
+def using_push(array, string)
+  return array.unshift(string)
+end
+
+def using_push(array)
+  return array.pop()
+end
+
+def using_push(array)
+  return array.pop(2)
+end
